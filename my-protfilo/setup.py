9789a448243ba980435a7a8b2c988e2aaa87c84e
Mr-kiran56 <kiranpunna815@gmail.com>
2026-01-02 21:46:08 +0530

    yes

---
+++ my-protfilo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "My-Protfilo",
+    version= "0.0.0",
+    author="Kiran",
+    author_email="kiranpunna58@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
