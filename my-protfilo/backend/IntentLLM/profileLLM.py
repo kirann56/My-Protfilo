ca8a8725601708831fe7cb07b992cc8709b89032
Mr-kiran56 <kiranpunna815@gmail.com>
2026-02-06 20:51:52 +0530

    first-cicd

--- my-protfilo/backend/IntentLLM/profileLLM.py
+++ my-protfilo/backend/IntentLLM/profileLLM.py
@@ -18,8 +18,13 @@ logger = logging.getLogger(__name__)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 BASE_DIR = Path(__file__).resolve().parent
-
-MODEL_PATH = BASE_DIR / "profileIntentLLM"
+MODEL_PATH = Path(
+    os.getenv(
+        "INTENT_MODEL_PATH",
+        "/app/backend/IntentLLM/profileIntentLLM"
+    )
+)
+# MODEL_PATH = BASE_DIR / "profileIntentLLM"
 LABEL_ENCODER_PATH = BASE_DIR / "label_encoder_p.pkl"
 
 
--- my-protfilo/backend/main.py
+++ my-protfilo/backend/main.py
@@ -12,7 +12,7 @@ from fastapi.responses import FileResponse
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-FILE_PATH ="B:/Protfilo/my-protfilo/src/assets/Kiran_Punna_AIML_Resume.pdf"
+FILE_PATH ="/app/backend/resume/Kiran_Punna_AIML_Resume.pdf"
 # --- LIFESPAN MANAGER (WINDOWS SAFE VERSION) ---
 @asynccontextmanager
 async def lifespan(app: FastAPI):
