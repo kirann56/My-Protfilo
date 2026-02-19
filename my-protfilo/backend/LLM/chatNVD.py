a11283697f165f0d37a3eda455d36b72244758ef
Mr-kiran56 <kiranpunna815@gmail.com>
2026-02-19 15:52:24 +0530

    Initial commit

--- my-protfilo/backend/LLM/chatNVD.py
+++ my-protfilo/backend/LLM/chatNVD.py
@@ -27,7 +27,7 @@ def get_graph_for_user(username: str):
         model="meta/llama-3.1-8b-instruct",
         temperature=0.1,
         max_completion_tokens=4096,
-        #streaming=True
+        streaming=True
     )
     retriever = get_retriever()
     prompt = unified_profile_prompt()
